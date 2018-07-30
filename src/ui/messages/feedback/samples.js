3df0c5d107c9cab82c9f9cffe6b3cb6d8a36f9f4
Michael Kantor <michael@layer.com>
2018-07-30 14:21:25 -0700

    Updated Samples

--- src/ui/messages/feedback/samples.js
+++ src/ui/messages/feedback/samples.js
@@ -9,22 +9,54 @@ model = new FeedbackModel({
 });
 model.send({ conversation: $("layer-conversation-view").conversation });
 
-new TextModel({text: "Feedback with custom action data"}).send({ conversation: $("layer-conversation-view").conversation });
+
+TextModel = Layer.Core.Client.getMessageTypeModelClass('TextModel');
+FeedbackModel = Layer.Core.Client.getMessageTypeModelClass('FeedbackModel');
+new TextModel({text: "Custom prompt"}).send({ conversation: $("layer-conversation-view").conversation });
 
 model = new FeedbackModel({
   enabledFor: Layer.client.user.id,
-  action: {
-    data: {hey: 'ho'}
-  }
+  prompt: "Hey Ho"
 });
 model.send({ conversation: $("layer-conversation-view").conversation });
 
 
 TextModel = Layer.Core.Client.getMessageTypeModelClass('TextModel');
 FeedbackModel = Layer.Core.Client.getMessageTypeModelClass('FeedbackModel');
+
+new TextModel({text: "Custom promptWait"}).send({ conversation: $("layer-conversation-view").conversation });
+model = new FeedbackModel({
+  enabledFor: Layer.client.user.id,
+  promptWait: "Hey Ho"
+});
+model.send({ conversation: $("layer-conversation-view").conversation });
+
+new TextModel({text: "Custom placeholder"}).send({ conversation: $("layer-conversation-view").conversation });
+model = new FeedbackModel({
+  enabledFor: Layer.client.user.id,
+  placeholder: "Hey Ho"
+});
+model.send({ conversation: $("layer-conversation-view").conversation });
+
+new TextModel({text: "Custom title"}).send({ conversation: $("layer-conversation-view").conversation });
+model = new FeedbackModel({
+  enabledFor: Layer.client.user.id,
+  title: "Hey Ho"
+});
+model.send({ conversation: $("layer-conversation-view").conversation });
+
+new TextModel({text: "Custom action"}).send({ conversation: $("layer-conversation-view").conversation });
+model = new FeedbackModel({
+  enabledFor: Layer.client.user.id,
+  action: {event: "open-url", data: {url: "https://layer.com"}}
+});
+model.send({ conversation: $("layer-conversation-view").conversation });
+
+TextModel = Layer.Core.Client.getMessageTypeModelClass('TextModel');
+FeedbackModel = Layer.Core.Client.getMessageTypeModelClass('FeedbackModel');
 ButtonsModel = Layer.Core.Client.getMessageTypeModelClass('ButtonsModel');
 
-new TextModel({text: "Feedback with Button with custom action data"}).send({ conversation: $("layer-conversation-view").conversation });
+new TextModel({text: "Feedback with Open Feedback Button"}).send({ conversation: $("layer-conversation-view").conversation });
 model = new ButtonsModel({
   contentModel: new FeedbackModel({
     enabledFor: Layer.client.user.id,
